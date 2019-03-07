f0557e9848a0b58082c02a1c26c71af6e9f9932a
Aboelnaga <aboelnaga@aboelnagas-mac.local>
2019-03-07 03:17:43 -0800

    Initial Commit

---
+++ medic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  medic
+//
+//  Created by Aboelnaga on 3/7/19.
+//  Copyright © 2019 graduation_project. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
